341a3ebae168f24134602cad8fbfd36d1a2376d9
kiranks726 <kiranks726@gmail.com>
2022-05-12 15:25:16 +0000

    add conditional statemnt for diff

--- test.sh
+++ test.sh
@@ -1,2 +1,15 @@
-npx sst diff --fail && echo "no diffs found" || echo "diffs found"
-echo "$?"
+a="Using stage: dev
+Preparing your SST app
+Building function cmd/handlers/movies/list/list.go
+Building function cmd/handlers/movies/create/create.go
+Building function cmd/handlers/movies/get/get.go
+Building function cmd/handlers/movies/update/update.go
+Building function cmd/handlers/movies/delete/delete.go
+Stack dev-ctx-kitchensink-go-movie-stack
+There were no differences"
+
+if [[ $a == *"no differences"* ]]; then
+  echo "It's there!"
+else
+    echo "Not there"
+fi
